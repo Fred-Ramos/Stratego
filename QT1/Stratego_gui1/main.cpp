ada34b10ea27f998e43289f29d7c2a82d1a8479b
Frederico <up201907180@up.pt>
2023-12-06 11:54:32 +0000

    Sourcetree

--- QT1/Stratego_gui1/main.cpp
+++ QT1/Stratego_gui1/main.cpp
@@ -12,21 +12,8 @@ int main(int argc, char *argv[])
     game = new Game();
 
     game->show();
-<<<<<<< HEAD
-    game->displayGameOver(); //start the game
     game->loginGame(); //start the game
     //game->displayMainMenu();
-=======
 
-    //game->displayGameOver(); //start the game
-
-<<<<<<< HEAD
-    //game->loginGame(); //start the game
-    game->displayMainMenu();
->>>>>>> 272cbbf9a6839fd87deea4620f7aa30f1b84015a
-=======
-    game->loginGame(); //start the game
-    //game->displayMainMenu();
->>>>>>> 660572f81de2dac00486afb821984f8e0e0b5105
     return a.exec();
 }
